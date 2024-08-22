4b1157ba8786de4589b504f2b7ab694e48b08b73
Hasan Divarci <hasandivarciuk@hotmail.com>
2024-08-22 21:26:40 +0100

    Unit Test implementations are completed and improved

--- QuickMapping.Tests/Tests/DefaultOptions/Models/CompanyViewModel.cs
+++ QuickMapping.Tests/Tests/DefaultOptions/Models/CompanyViewModel.cs
@@ -2,6 +2,8 @@
 public class CompanyViewModel
 {
     public string Description { get; set; } = null!;
+
+    public UserViewModel Director { get; set; } = null!;
     public List<UserViewModel> Employees { get; set; } = null!;
 
 }
