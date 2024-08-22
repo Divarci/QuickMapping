d191248cb0598ccfaa76409bdc1aac8235fac88c
Hasan Divarci <hasandivarciuk@hotmail.com>
2024-08-22 18:59:42 +0100

    Case Sensitive Option is added

--- QuickMapping.Tests/Tests/DefaultOptions/Models/UserViewModel.cs
+++ QuickMapping.Tests/Tests/DefaultOptions/Models/UserViewModel.cs
@@ -1,4 +1,4 @@
-﻿namespace QuickMapping.Tests.Models;
+﻿namespace QuickMapping.Tests.Tests.DefaultOptions.Models;
 public class UserViewModel()
 {
     public string Fullname { get; set; } = null!;
--- QuickMapping/Extensions/ServiceExtension.cs
+++ QuickMapping/Extensions/ServiceExtension.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using QuickMapping.Abstract;
 using QuickMapping.Concrete;
+using QuickMapping.Options;
 
 namespace QuickMapping.Extensions;
 public static class ServiceExtension
@@ -8,6 +9,15 @@ public static class ServiceExtension
     public static IServiceCollection AddQuickMapping(this IServiceCollection service)
     {
         service.AddScoped<IQuickMapper,QuickMapper>();
+        return service;
+    }
+
+    public static IServiceCollection AddQuickMapping(this IServiceCollection service, Action<MappingOptions> configureOptions)
+    {
+        var options = new MappingOptions();
+        configureOptions(options);
+
+        service.AddScoped<IQuickMapper>(sp => new QuickMapper(options));
         return service;
     }
 }
