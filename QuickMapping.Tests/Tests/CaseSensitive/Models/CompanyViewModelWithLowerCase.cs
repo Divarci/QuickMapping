54a3311cb8ca7d82e5ee32475ff647242eb1aa90
Hasan Divarci <hasandivarciuk@hotmail.com>
2024-08-22 21:24:02 +0100

    Unit Test implementations are completed and improved

---
+++ QuickMapping.Tests/Tests/CaseSensitive/Models/CompanyViewModelWithLowerCase.cs
@@ -0,0 +1,8 @@
+﻿namespace QuickMapping.Tests.Tests.CaseSensitive.Models;
+public class CompanyViewModelWithLowerCase
+{
+    public string DESCRIPTION { get; set; } = null!;
+
+    public UserViewModelWithLowerCase director { get; set; } = null!;
+    public List<UserViewModelWithLowerCase> EmployeeS { get; set; } = null!;
+}
