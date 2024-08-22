8c6ffc0e75c6f7d03b5ef19773fb4423672637ab
Hasan Divarci <hasandivarciuk@hotmail.com>
2024-08-22 21:26:40 +0100

    Unit Test implementations are completed and improved

--- QuickMapping.Tests/Entities/Company.cs
+++ QuickMapping.Tests/Entities/Company.cs
@@ -1,8 +1,22 @@
-﻿namespace QuickMapping.Tests.Entities;
-public class Company(int id, string description, List<User> employees)
-{
-    public int Id { get; set; } = id;
-    public string Description { get; set; } = description;
+﻿using System.IO;
 
-    public List<User> Employees { get; set; } = employees;
+namespace QuickMapping.Tests.Entities;
+public class Company
+{
+    public Company(int id, string description, List<User> employees, User director)
+    {
+        Id = id;
+        Description = description;
+        Employees = employees;
+        Director = director;
+
+    }
+
+    public int Id { get; set; }
+    public string Description { get; set; }
+
+    public User Director { get; set; }
+    public List<User> Employees { get; set; }
+
+    
 }
